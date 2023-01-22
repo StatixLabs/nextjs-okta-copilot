3bbdb5b3acec2f3d3d982fe834531b4dc4a92572
Jake Alves <jake@statixlabs.com>
2023-01-21 19:04:08 -0500

    initial commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import Okta from 'next-auth/providers/okta'
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export const authOptions: NextAuthOptions = {
+  // https://next-auth.js.org/configuration/providers/oauth
+  providers: [
+    Okta({
+      clientId: process.env.OKTA_OAUTH2_CLIENT_ID as string,
+      clientSecret: process.env.OKTA_OAUTH2_CLIENT_SECRET as string,
+      issuer: process.env.OKTA_OAUTH2_ISSUER as string,
+    }),
+  ],
+  theme: {
+    colorScheme: "light",
+  },
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "admin"
+      return token
+    },
+  },
+}
+
+export default NextAuth(authOptions)
